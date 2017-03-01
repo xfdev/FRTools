33e79461917a1cd78f92277c821fd85a3fcab7a3
sonny <anangcocoy@gmail.com>
2017-03-01 14:51:11 +0800

    更新网络请求。。。。

--- FRTools/Helps/FRHttpService.m
+++ FRTools/Helps/FRHttpService.m
@@ -14,6 +14,53 @@
 #import <YYModel.h>
 #import "FRHttpError.h"
 
+
+
+@interface FRHTTPSessionManager : AFHTTPSessionManager
+
++ (instancetype)sharedInstance;
+
+@end
+
+/**
+ 关于 manager 使用不当内存泄漏的问题：
+ 
+ https://github.com/AFNetworking/AFNetworking/issues/3293
+ Do not use a class func.Make sure your manager is a global variable
+ 
+ https://github.com/AFNetworking/AFNetworking/issues/3787
+ Leaks are attributed to where an object is created, not where it is leaked. You should be looking for where you leak these objects, not where you allocate them.
+ */
+@implementation FRHTTPSessionManager
+
+static FRHTTPSessionManager *manager = nil;
+
++ (instancetype)sharedInstance {
+    
+    static dispatch_once_t once;
+    dispatch_once(&once, ^{
+        AFSecurityPolicy *securityPolicy = [AFSecurityPolicy defaultPolicy];
+        securityPolicy.allowInvalidCertificates = YES;  // 是否允许无效证书（也就是自建的证书），默认为NO
+        securityPolicy.validatesDomainName = NO;        // 是否需要验证域名，默认为YES；
+        
+        manager = [[self alloc] init];
+        manager.requestSerializer = [AFJSONRequestSerializer serializer];
+        manager.securityPolicy  = securityPolicy;
+        manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"text/html", nil];
+//        manager.requestSerializer = [AFJSONRequestSerializer serializer];// 请求为 json 数据，`Content-Type` encoded request `application/json`.
+//        manager.requestSerializer = [AFHTTPRequestSerializer serializer];// 默认为此选项，请求为 From 表单
+        
+    });
+    return manager;
+}
+
+@end
+
+/**
+ 取消当前会话：
+ https://github.com/AFNetworking/AFNetworking/issues/3546
+ */
+
 @implementation FRHttpService
 
 static FRHttpService *service = nil;
@@ -72,24 +119,16 @@ static FRHttpService *service = nil;
     NSString *URL = method;
     
     // 对参数进行处理
-    NSMutableDictionary *tmpDic = [NSMutableDictionary dictionaryWithDictionary:_params];
+    NSMutableDictionary *postParams = [NSMutableDictionary dictionaryWithDictionary:_params];
     if (dic) {
-        [tmpDic addEntriesFromDictionary:dic];
+        [postParams addEntriesFromDictionary:dic];
     }
     
-    NSLog(@"\n请求数据：method = %@,\njsonDic = %@,\nURL = %@\n\n",method,tmpDic,URL);
+    NSLog(@"\n请求数据：method = %@,\njsonDic = %@,\nURL = %@\n\n",method,postParams,URL);
     
-    AFSecurityPolicy *securityPolicy = [AFSecurityPolicy defaultPolicy];
-    securityPolicy.allowInvalidCertificates = YES;  // 是否允许无效证书（也就是自建的证书），默认为NO
-    securityPolicy.validatesDomainName = NO;        // 是否需要验证域名，默认为YES；
+    FRHTTPSessionManager *manager = [FRHTTPSessionManager sharedInstance];
     
-    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
-    manager.securityPolicy  = securityPolicy;
-    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"text/html", nil];
-//    manager.requestSerializer = [AFJSONRequestSerializer serializer];// 请求为 json 数据，`Content-Type` encoded request `application/json`.
-//    manager.requestSerializer = [AFHTTPRequestSerializer serializer];// 默认为此选项，请求为 From 表单
-    
-    [manager POST:URL parameters:tmpDic progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+    [manager POST:URL parameters:postParams progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
         
         NSLog(@"\n请求成功：method = %@,\nresponseObject = %@",method,[FRUtils jsonStringWithjsonObject:responseObject]);
         
